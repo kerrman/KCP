938e10797dbac344d239a216698977c3e8a9a0f4
Kerrman <40134363+kerrman@users.noreply.github.com>
2018-09-22 19:53:14 -0700

    Trying to get initial commit and create repository from eclipse, trying again

---
+++ src/server/ConnectionHandler.java
@@ -0,0 +1,15 @@
+package server;
+
+public class ConnectionHandler implements Runnable {
+
+	public ConnectionHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
